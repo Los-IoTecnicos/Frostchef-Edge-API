58e2d0bf2a77119e77ae3f2134e1901fd150e59d
Prog-san <DieSanR@gmail.com>
2024-11-02 07:55:03 -0500

    feat: Add OpenAPI Documentation

--- frostchef/src/main/java/com/losiotecnicos/frostchef/domain/models/Sensor.java
+++ frostchef/src/main/java/com/losiotecnicos/frostchef/domain/models/Sensor.java
@@ -1,11 +1,10 @@
 package com.losiotecnicos.frostchef.domain.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 @Entity
+@Table(name = "sensors")
 public class Sensor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
