7038762a1f8b52911186a5c46605e56b0937234e
Die-san <74322553+Prog-san@users.noreply.github.com>
2024-11-02 05:58:43 -0500

    Initial project

---
+++ frostchef/src/main/java/com/losiotecnicos/frostchef/FrostchefApplication.java
@@ -0,0 +1,13 @@
+package com.losiotecnicos.frostchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrostchefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrostchefApplication.class, args);
+	}
+
+}
---
+++ frostchef/src/test/java/com/losiotecnicos/frostchef/FrostchefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losiotecnicos.frostchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrostchefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
