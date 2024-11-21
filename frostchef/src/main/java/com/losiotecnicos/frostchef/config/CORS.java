e3341220d3fb811adf0b993873bb0bfce615d4b6
Prog-san <DieSanR@gmail.com>
2024-11-21 05:13:00 -0500

    hotfix: updated credentials

--- frostchef/src/main/java/com/losiotecnicos/frostchef/config/CORS.java
+++ frostchef/src/main/java/com/losiotecnicos/frostchef/config/CORS.java
@@ -18,7 +18,7 @@ public class CORS {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("*")
+                            .allowedOrigins("http://frostchef-edge-api-production.up.railway.app:8080")
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                             .allowedHeaders("*")
                             .allowCredentials(true);
