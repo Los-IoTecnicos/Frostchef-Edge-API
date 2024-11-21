2ba2afa8061c23d658b7c5a37fa8971bfadbef06
Prog-san <DieSanR@gmail.com>
2024-11-21 06:02:05 -0500

    fix: update CORS.java

--- frostchef/src/main/java/com/losiotecnicos/frostchef/config/CORS.java
+++ frostchef/src/main/java/com/losiotecnicos/frostchef/config/CORS.java
@@ -1,8 +1,14 @@
 package com.losiotecnicos.frostchef.config;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,5 +34,17 @@ public class CORS {
             }
         };
     }
+
+    @ControllerAdvice
+    public class GlobalExceptionHandler {
+        @ResponseStatus(HttpStatus.OK)
+        @RequestMapping(method = RequestMethod.OPTIONS)
+        public void handleOptionsRequest(HttpServletResponse response) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        }
+    }
+
 }
 
